9c465032ec8f9cd05336a0705292b2dff090e4cc
Stepan <108026663+stepan95@users.noreply.github.com>
2023-02-16 08:37:44 +0200

    Add files via upload

--- js/brush.js
+++ js/brush.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function() {
 	const brush = document.querySelectorAll('.brush');
 	// Лічильник збільшення до максимум
 	function brushUp(brush) {
-		const time = (900 / Number(brush.dataset.brushmax));
+		const time = (100000 / Number(brush.dataset.brushmax)/10);
 		const id = setInterval(function() {
 			if (Number(brush.dataset.brushmax) > brush.textContent) brush.textContent++;
 			if (brush.dataset.brushmax == brush.textContent) clearInterval(id);
@@ -17,7 +17,7 @@ window.addEventListener('load', function() {
 				caunt ++;
 			}
 		}
-		// Видаляємо скрол подію скрол коли всі лічильники показують доступне максимальне число
+		// Видаляємо подію скрол коли всі лічильники показують доступне максимальне число
 		if (caunt == brush.length) {
 			window.removeEventListener('scroll', scroll);
 		}
